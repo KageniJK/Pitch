e5c79f736867158ddbdcc40f0cf2d76a7c6bb545
James Kageni <james.kageni@gmail.com>
2018-09-07 21:17:59 +0300

    added the config classes and options

---
+++ config.py
@@ -0,0 +1,31 @@
+class Config:
+    """
+    class that set the general config
+    """
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    """
+    class that sets up production config
+    """
+
+    pass
+
+
+class DevConfig(Config):
+    """
+    class that sets up the development config
+    """
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://qagz:password@localhost/pitch'
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+}

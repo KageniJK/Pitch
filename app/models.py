474ace059712aa412f7c2313b817db43ab0296f6
James Kageni <james.kageni@gmail.com>
2018-09-09 19:20:38 +0300

    added the categories class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from . import db
+from . import db, login_manager
 from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(UserMixin, db.model):
@@ -9,13 +11,28 @@ class User(UserMixin, db.model):
 
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.column(db.String(255), index=True)
     email = db.Column(db.String(255), index=True)
     password_hashed = db.Column(db.String(255))
     avatar = db.Column(db.String)
 
-    # toDo add the relationships between tables
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hashed = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hashed, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
 
 class Pitch(db.model):
     """
@@ -24,10 +41,13 @@ class Pitch(db.model):
 
     __tablename__='pitches'
 
-    pitch_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     time_posted = db.Column(db.DateTime, default=datetime.utcnow)
     title = db.Column(db.String(255))
     pitch_actual = db.Column(db.String)
+
+
+
 
     def save_pitch(self):
         db.session.add(self)
@@ -37,3 +57,17 @@ class Pitch(db.model):
     def get_pitches(cls, id):
         pitches = Pitch.query.filter_by(category_id).all()
         return pitches
+
+class Categeory(db.model):
+    """
+    class that defines the categories of the pitches
+    """
+
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='category', lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'

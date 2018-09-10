fd479803939bc666822b22ff3394a5b3ecee9ff2
James Kageni <james.kageni@gmail.com>
2018-09-10 05:23:31 +0300

    added the welcome email

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
 from . import auth
+from ..email import mail_message
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -36,6 +37,8 @@ def register():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to Pitch", "email/welcome_user", user.email, user=user)
 
         return redirect(url_for('auth.login'))
     title = "New Account"
